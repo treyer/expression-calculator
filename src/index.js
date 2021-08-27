480f0c9d3ee68a577962e8fdbefa577494011bd7
treyer <nemereno_by@mail.ru>
2021-08-27 23:34:41 +0300

    feat: implement calculation of simple expressions without brackets

--- src/index.js
+++ src/index.js
@@ -4,7 +4,103 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    
+    expr = expr.replace(/\s/g, '');//delete spaces
+
+    if (expr.indexOf('(') === -1 && expr.indexOf(')') === -1){
+        return calculateSimpleExpression(expr);
+    } else {
+        //check if expression has correct brackets
+        
+        //в цикле - ищем вложенности скобок (...) и считаем выражения внутри, заменяем в строке (...) на число и начинаем сначала до 
+        // того, как скобок не останется 
+    }
+
+    
+}
+
+//calculates expression without parenthesis
+let calculateSimpleExpression = (simpleStr) => {
+
+    let indexMult, indexDev, indexSubtr, indexAdd;
+
+    while ((indexMult = simpleStr.indexOf('*')) > -1 || (indexDev = simpleStr.indexOf('/')) > -1 ||
+            (indexSubtr = simpleStr.indexOf('-')) > -1 || (indexAdd = simpleStr.indexOf('+')) > -1){
+        if (indexMult > -1 && indexDev > -1){
+            if (indexMult < indexDev){
+                simpleStr = makeOperation(simpleStr, "*", indexMult);
+            } else {
+                simpleStr = makeOperation(simpleStr, "/", indexDev);
+            }
+        } else if (indexMult > -1){
+            simpleStr = makeOperation(simpleStr, "*", indexMult);
+        } else if (indexDev > -1) {
+            simpleStr = makeOperation(simpleStr, "/", indexDev);
+        } else if (indexSubtr > -1 && indexAdd > -1){
+            if (indexSubtr < indexAdd){
+                simpleStr = makeOperation(simpleStr, "-", indexSubtr);
+            } else {
+                simpleStr = makeOperation(simpleStr, "+", indexAdd);
+                console.log(simpleStr);
+            }
+        } else if (indexSubtr > -1){
+            simpleStr = makeOperation(simpleStr, "-", indexSubtr);
+        } else if (indexAdd > -1){
+            simpleStr = makeOperation(simpleStr, "+", indexAdd);
+        }
+
+        indexMult = simpleStr.indexOf('*');
+        indexDev = simpleStr.indexOf('/');
+        indexSubtr = simpleStr.indexOf('-');
+        indexAdd = simpleStr.indexOf('+');
+    }
+
+    return simpleStr;
+
+}
+
+let makeOperation = (str, operationSign, operatorPosition) => {
+    let firstOperand = "";
+    let secondOperand = "";
+    let strCutBegin;
+    let strCutEnd;
+    let operationResult;
+
+    //get first operand
+    let i = operatorPosition - 1;
+    while(i > -1){
+        if (str[i] !== '*' && str[i] !== '/' && str[i] !== '+' && str[i] !== '-'){
+            strCutBegin = i;
+        } else {
+            break;
+        }
+        i--;
+    }
+    firstOperand = str.substring(strCutBegin, operatorPosition);
+    
+    //get second operand
+    i = operatorPosition + 1;
+    while(i < str.length){
+        if (str[i] !== '*' && str[i] !== '/' && str[i] !== '+' && str[i] !== '-'){
+            secondOperand += str[i];
+            strCutEnd = i;
+        } else {
+            break;
+        }
+        i++;
+    }
+
+    if (operationSign === "*") operationResult = +firstOperand * +secondOperand;
+    //TODO: devision by 0 situation
+    if (operationSign === "/") operationResult = +firstOperand / +secondOperand;
+    if (operationSign === "+") operationResult = +firstOperand + +secondOperand;
+    if (operationSign === "-") operationResult = +firstOperand - +secondOperand;
+
+    if (strCutBegin === 0 && strCutEnd === str.length - 1) return String(operationResult);
+    if (strCutBegin === 0) return String(operationResult) + str.substring(strCutEnd + 1);
+    if (strCutEnd === str.length - 1) return str.substring(0, strCutBegin) + String(operationResult);
+
+    return str.substring(0, strCutBegin) + String(operationResult) + str.substring(strCutEnd + 1);
 }
 
 module.exports = {
